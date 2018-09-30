a4baad5a4920d4933e4544cbb77fb8082ef67514
Andrés Castellanos <andres.cv@galileo.edu>
2018-09-29 21:05:58 -0600

    add autograder script

---
+++ pa4-grading.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+
+import os
+import gzip
+import pickle
+from autograder import cprint
+from autograder import colored
+
+
+# max score
+SCORE = 63
+
+
+# creates grading and grading/test-output directories
+def start():
+    os.system('clear')
+    os.system('rm -rf ' + os.path.join('grading'))
+    os.system('rm -rf ' + os.path.join('grading', 'test-output'))
+    os.system('mkdir ' + os.path.join('grading'))
+    os.system('mkdir ' + os.path.join('grading', 'test-output'))
+
+
+# gets terminal cols dim
+def cols():
+    rows, columns = os.popen('stty size', 'r').read().split()
+    return int(columns)
+
+
+# creates an output file
+def create(name, data):
+    if not os.path.exists(os.path.join('grading', name)):
+        f = open(os.path.join('grading', name), 'w')
+        f.write(data)
+        f.close()
+
+
+# executes a test
+def execute(name):
+    test = os.path.join('grading', name + '.cl')
+    testout = os.path.join('grading', '.tmp.s')
+    os.system('./mycoolc -o ' + testout + ' ' + test)
+    os.system('vsim -nocolor -notitle ' + testout + ' >> grading/.tmp 2>&1')
+    testout = open('grading/.tmp', 'r')
+    testresult = testout.read().strip()
+    testout.close()
+    return testresult
+
+
+# copy result
+def copy(name):
+    os.system('cp grading/.tmp grading/test-output/' + name + '.cl.out')
+
+
+# calls diff
+def diff(name):
+    cmd = 'diff grading/%s.cl.out grading/test-output/%s.cl.out' % (name, name)
+    cmd += ' > grading/test-output/%s.diff' % name
+    os.system(cmd)
+
+
+# clear temp files
+def clear():
+    os.system('rm -rf grading/.tmp')
+    os.system('rm -rf grading/.tmp.s')
+
+
+# main script
+def main():
+    f = gzip.open(os.path.join('autograder', 'data.pkl.gz'), 'rb')
+    data = pickle.load(f)
+    f.close()
+    start()
+    print('')
+    cprint('Autograder'.center(cols(), ' '), color='blue')
+    print('')
+    print('')
+    os.system('make cgen > /dev/null 2>&1')
+    grade = 0
+    for sample in data:
+        # unpack test data
+        input, output, result, points, name, desc = sample
+        # create test input
+        create(name + '.cl', input)
+        # create test expected result
+        create(name + '.cl.out', result)
+        # create test expected .s
+        create(name + '.cl.s', output)
+        # run test
+        uresult = execute(name)
+        # compare
+        if uresult != result:
+            copy(name)
+            diff(name)
+            info = colored('-%d' % points, 'red')
+            info += colored('(%s)' % name, 'cyan').center(37, ' ')
+            info += colored(' %s' % desc, 'white')
+            print(info)
+        else:
+            grade += points
+        # clear output
+        clear()
+    # chek score
+    if grade == SCORE:
+        t1 = colored('\n[GC Test Case: lam-gc.cl ', 'white')
+        t1 += colored('OK', 'green') + colored(']', 'white')
+        print(t1)
+        t1 = colored('[GC Test Case: simple-gc.cl ', 'white')
+        t1 += colored('OK', 'green') + colored(']', 'white')
+        print(t1)
+    print('\n')
+    cprint('=> You got a score of %d out of 63.' % grade, color='yellow')
+
+
+if __name__ == '__main__':
+    main()
